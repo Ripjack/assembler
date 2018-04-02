3daae0f27348b8fa7860c84b22d3be4426b892b9
Ripjack <theranslinkard@gmail.com>
2018-04-02 10:35:59 -0500

    reformat
    Signed-off-by: Ripjack <theranslinkard@gmail.com>

--- assembler.py
+++ assembler.py
@@ -1,18 +1,20 @@
-#python versoin 3.6
+# python versoin 3.6
 with open('assembly', 'r') as file:
-	data = file.read()
-	lines = data.split(";\n")
-	commands = {
-	'add': '0000 0000',
-	'set': '0000 0000',
-	'out': '0000 0000'
-	}
-	cmds = {}
-	for cmd in lines:
-		if cmd == '':
-			cmds[format(len(cmds), 'x')] = ["EOF"]
-			break
-		line = cmd.split(" ")
-		addr = line[0]
-		cmds[addr] = line[1:]
-	print(cmds)
+    data = file.read()
+    lines = data.split(";\n")
+    commands = {
+        'add': '0000 0000',
+        'set': '0000 0000',
+        'out': '0000 0000'
+    }
+    registers = {
+        '':''
+    }
+    cmds = {}
+    for cmd in lines:
+        if cmd == '':
+            cmds[format(len(cmds), 'x')] = ["EOF"]
+            break
+        line = cmd.split(" ")
+        addr = line[0]
+        instr = line[1]
